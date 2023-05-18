c8ffd33c6ef3908d1ecd6b06970cab5a499961ac
samratbatra <samratbatra8@gmail.com>
2023-05-18 12:43:43 +0530

    "Logging"

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepo customerRepo;
 
@@ -18,13 +22,18 @@ public class CustomerService {
 
     public List<Customer> getCustomers() {
 
+        LOGGER.info("getCustomers was called");
         return customerRepo.getCustomers();
     }
 
 
     public Customer getCustomer(Long id) {
+        return customerRepo.getCustomers().stream().filter(customer -> customer.getId() == id).findFirst().orElseThrow(() -> {
+            NotFoundException notFoundException = new NotFoundException("Customer with id " + id + " not found");
+            LOGGER.error("Error in getting  customer id {}", id,
+                    notFoundException);
 
-        return customerRepo.getCustomers().stream().filter(customer -> customer.getId() == id).findFirst().orElseThrow(() ->
-            new NotFoundException("Customer with id " + id + " not found") );
+            return notFoundException;
+        });
     }
 }
