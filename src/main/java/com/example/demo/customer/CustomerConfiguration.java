a4b94afba244f35365db9ff7b4ef0cc40b8dd3de
samratbatra <samratbatra8@gmail.com>
2023-05-19 02:11:39 +0530

    " Actuator, Value , Environment,Configuration Properties"

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,22 +1,37 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
 
-@Value("${app.useFakeCustomerRepo:false}")
+    @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Autowired
+    private Environment environment;
+
+    @Value("${info.app.name}")
+    private String companyName;
+
     @Bean
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(InfoApp infoApp){
 
         return args -> {
 
             System.out.println("Command Line runner");
+
+            System.out.println(companyName);
+
+            System.out.println(environment.getProperty("info.app.version"));
+
+            System.out.println(infoApp);
         };
     }
 
